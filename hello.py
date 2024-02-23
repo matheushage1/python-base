318a0973031b69a7816a0f03c4131982358eaad3
matheushage1 <matheushagecontato@gmail.com>
2024-02-23 12:33:33 -0300

    adding new knowledgment and also a dir

--- hello.py
+++ hello.py
@@ -1,1 +1,19 @@
-print("I am starting to learn Python full focused")
+"""Hello World multilínguas.
+
+Dependendo da linguagem configurada no ambiente, 
+o programa exibe a mensagem correspondente.
+
+Como usar: Tenha a variável LANG configurada em seu ambiente.
+
+  export LANG=pt_br
+
+Execução: Executar o código do arquivo (python3 <nome-do-arquivo>).
+"""
+# Dunders
+__version__ = "0.0.1"
+__author__ = "Matheus"
+__license__ = "unlicense"
+
+# Este programa imprime na tela "hello-world".
+print("Hello, World!")
+
